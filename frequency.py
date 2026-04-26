c0a43a56b25887755752ebc705a41647aa7917d5
Westley Hunter <westleyhunter@icloud.com>
2026-04-25 20:36:41 -0400

    Frequency dictionary generation completed

--- frequency.py
+++ frequency.py
@@ -1,6 +1,11 @@
 import requests
+from pathlib import Path
 
 word_freq = {}
+all_guesses = Path('valid-wordle-words.txt').read_text().splitlines()
+offensive_words = Path('badwords.txt').read_text().splitlines()
+#TODO: Add hash check to ensure up to date offensive words txt file
+legal_guesses = list(set(all_guesses) - set(offensive_words))
 
 def get_freq(word):
     # Creates the url to call the datamuse api with the desired word
@@ -9,5 +14,12 @@ def get_freq(word):
     # Indexes the word frequency, converts it to a float, and adds it to the global frequency list
     word_freq[word] = float(payload[0]['tags'][0][2:])
 
-get_freq('power')
-print(word_freq.get('power'))
+def get_all_freq(guess_list):
+    for i in range(len(guess_list)):
+        get_freq(guess_list[i])
+
+    total_freq = sum(word_freq.values())
+    for key, value in word_freq.items():
+        word_freq[key] = value / total_freq
+
+
