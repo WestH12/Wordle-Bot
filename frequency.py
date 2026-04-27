bb4516552eb54258e9a31c232d7eb9b998ab5157
Westley Hunter <westleyhunter@icloud.com>
2026-04-27 01:57:42 -0400

    Fixed frequency generation w/ log normalization and no api calling

--- frequency.py
+++ frequency.py
@@ -2,6 +2,7 @@ import requests
 from pathlib import Path
 import hashlib
 import pandas as pd
+import math
 
 word_freq = {}
 all_guesses = Path('files/valid-wordle-words.txt').read_text().splitlines()
@@ -29,13 +30,18 @@ def get_freq():
     freq_list_df['count'] = freq_list_df['count'].astype(float)
     freq_list = freq_list_df[freq_list_df.index.str.len() == 5]['count'].to_dict()
     for word in legal_guesses:
-        word_freq[word] = freq_list.get(word, .1)
+        word_freq[word] = freq_list.get(word, min(freq_list.values()))
 
-    total_freq = sum(word_freq.values())
-    for key, value in word_freq.items():
-        word_freq[key] = value / total_freq
+    log_freq = {word: math.log(freq) for word, freq in word_freq.items()}
+    min_log = min(log_freq.values())
+    max_log = max(log_freq.values())
+    epsilon = 1e-4
+    for word in log_freq:
+        normalized = (log_freq[word] - min_log) / (max_log - min_log)
+
+        word_freq[word] = epsilon + (1 + epsilon) * normalized
 
 
 check_hashes()
 get_freq()
-# print(word_freq)
+print(max(word_freq.values()))
