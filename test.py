5b898813b36f927463a6a732b700d7956819a97b
Westley Hunter <westleyhunter@icloud.com>
2026-05-09 22:31:46 -0400

    Small changes

--- test.py
+++ test.py
@@ -9,16 +9,17 @@ word_freq = frequency.get_freq()
 def test_bot(legal_guesses):
     accuracy = 0
     missed = 0
+    missed_list = []
 
     print("Testing Westley's Wordle Bot")
 
     for i, secret in enumerate(legal_guesses):
         guesses_list = legal_guesses
         guess = 1
-        if (i + 1) % (math.floor(len(legal_guesses) * .1)) == 0 and i != 0:
+        if (i + 1) % (math.floor(len(legal_guesses) * .01)) == 0 and i != 0:
             perc = (i + 1) / len(legal_guesses) * 100
-            print(f'Testing: {perc}% | Average Score:  - ' + str(
-                round(accuracy / (i + 1), 2)) + f'- # of Missed Words: {missed}')
+            print(f'Testing: {perc:.2f}% | Average Score:  - ' + str(
+                round(accuracy / (i + 1), 2)) + f' - # of Missed Words: {missed}')
 
         while True:
             if guess == 1:
@@ -26,6 +27,7 @@ def test_bot(legal_guesses):
             elif guess == 7:
                 accuracy += 8
                 missed += 1
+                missed_list.append(secret)
                 break
             else:
                 word = entropy.tot_guess_eval(guesses_list)
@@ -40,6 +42,9 @@ def test_bot(legal_guesses):
 
     fin_accuracy = accuracy / len(legal_guesses)
     print(f'Final average score: {fin_accuracy}')
+    print("Missed Words:")
+    for word in missed_list:
+        print(word)
 
 
 test_bot(frequency.legal_guesses)
